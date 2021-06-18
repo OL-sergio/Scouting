bf761681957f8af6e09cdc8bca85e178c8853467
paulorsfaria <83827183+paulorsfaria@users.noreply.github.com>
2021-06-18 17:47:02 +0100

    add create scout and staff

---
+++ app/src/main/java/ipca/am/scounting/main/CreateScout.kt
@@ -0,0 +1,12 @@
+package ipca.am.scounting.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ipca.am.scounting.R
+
+class CreateScout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_scout)
+    }
+}
